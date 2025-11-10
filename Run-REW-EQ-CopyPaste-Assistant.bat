0a54ace77f1abae72e9a459d50918ed09cd294f6
dahack <dahaque@gmail.com>
2025-11-11 00:37:46 +0100

    .bat file drive and path change

--- Run-REW-EQ-CopyPaste-Assistant.bat
+++ Run-REW-EQ-CopyPaste-Assistant.bat
@@ -1,3 +1,3 @@
 echo "Running REW-EQ-CopyPaste-Assistant.ps1..."
-cd "%~dp0"
+cd /d "%~dp0"
 powershell -executionpolicy remotesigned -command "Get-ChildItem -path .\* -include '*.ps1','*.psm1' -Recurse | % {Unblock-File $_.FullName}; . .\REW-EQ-CopyPaste-Assistant.ps1"
