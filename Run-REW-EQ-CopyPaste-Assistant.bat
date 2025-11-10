4a1ee08f8117d1a81c6518bd87e1bf66b6e33c81
dahack <dahaque@gmail.com>
2025-11-11 00:31:51 +0100

    Modules unblock added to .bat file

--- Run-REW-EQ-CopyPaste-Assistant.bat
+++ Run-REW-EQ-CopyPaste-Assistant.bat
@@ -1,2 +1,2 @@
 echo "Running REW-EQ-CopyPaste-Assistant.ps1..."
-powershell -executionpolicy remotesigned -command "unblock-file .\REW-EQ-CopyPaste-Assistant.ps1; . .\REW-EQ-CopyPaste-Assistant.ps1"
+powershell -executionpolicy remotesigned -command "Get-ChildItem -path .\* -include '*.ps1','*.psm1' -Recurse | % {Unblock-File $_.FullName}; . .\REW-EQ-CopyPaste-Assistant.ps1"
