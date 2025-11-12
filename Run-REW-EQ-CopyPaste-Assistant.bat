a7642d4575a6bbf3b644ed6fcc72efe0387d1469
Ivan Bakhmutov <dahaque@gmail.com>
2025-11-12 09:40:41 +0100

    changed quotes

--- Run-REW-EQ-CopyPaste-Assistant.bat
+++ Run-REW-EQ-CopyPaste-Assistant.bat
@@ -1,3 +1,3 @@
 echo "Running REW-EQ-CopyPaste-Assistant.ps1..."
 cd /d "%~dp0"
-powershell -executionpolicy remotesigned -command "Get-ChildItem -path .\* -filter "*" -Recurse | %% {Unblock-File $_.FullName}; . .\REW-EQ-CopyPaste-Assistant.ps1"
+powershell -executionpolicy remotesigned -command "Get-ChildItem -path .\* -filter '*' -Recurse | %% {Unblock-File $_.FullName}; . .\REW-EQ-CopyPaste-Assistant.ps1"
