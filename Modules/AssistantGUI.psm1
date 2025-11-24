14dd23047ca9b1aa96d3afdd7127a704dfcdb68e
Ivan Bakhmutov <dahaque@gmail.com>
2025-11-24 16:17:53 +0100

    admin priv error msg on ok btn click

--- Modules/AssistantGUI.psm1
+++ Modules/AssistantGUI.psm1
@@ -885,19 +885,16 @@ Function Show-SelectProfileGui {
     $ProfileEditGUI.FindName("OKBTN").Add_Click({
             $selectedProfileFileName = $ProfileEditGUI.FindName("ProfileList").SelectedItem
             if($result.AdminRightsRequired -eq "true"){
-               <#if (-not Get-RunningAsAdminFlag) {
-                    try {
+               if (-not (Get-RunningAsAdminFlag)) {
+              
                         Add-Type -AssemblyName PresentationCore,PresentationFramework -ErrorAction SilentlyContinue
                         $ButtonType   = [System.Windows.MessageBoxButton]::OK
                         $MessageIcon  = [System.Windows.MessageBoxImage]::Error
                         $MessageBody  = "Selected DSP profile requires administrative privileges. Please run the script as an administrator."
                         $MessageTitle = "Administrative Privileges Required"
                         [System.Windows.MessageBox]::Show($MessageBody, $MessageTitle, $ButtonType, $MessageIcon) | Out-Null
-                    }
-                    catch {
-                        Write-Host "Selected DSP profile requires administrative privileges. Please run the script as an administrator." -ForegroundColor Red
-                    }
-                }#>
+                        return
+                }
             }
             if ($null -ne $selectedProfileFileName) {
                 $result.Action = "Open"
